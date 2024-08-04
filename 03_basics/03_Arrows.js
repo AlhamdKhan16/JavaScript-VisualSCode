5f03579199dca3ae8a8f5d4c98861b92d0c7d65e
AlhamdKhan16 <alhamdkhan2003@gmail.com>
2024-08-04 06:57:01 +0000

    arrows

--- 03_basics/03_Arrows.js
+++ 03_basics/03_Arrows.js
@@ -1,18 +1,41 @@
-const user = {
-    username: "Alhamd",
-    price: 1000,
-    welcomeMessage: function() {
-        console.log(`${this.username}, welcome to the website`); 
-        console.log(this);
-    }
-}
-// user.welcomeMessage();
-// user.username = "Umar"
-// user.welcomeMessage();
-console.log(this);
+// const user = {
+//     username: "Alhamd",
+//     price: 1000,
+//     welcomeMessage: function() {
+//         console.log(`${this.username}, welcome to the website`); 
+//         console.log(this);
+//     }
+// }
+// // user.welcomeMessage();
+// // user.username = "Umar"
+// // user.welcomeMessage();
+// console.log(this);
 
-function chai(){
-    let username = "Alhamd"
-    console.log(this);
-}
-chai()
+// function chai(){
+//     let username = "Alhamd"
+//     console.log(this);
+// }
+// chai()
+
+// const chai = function () { 
+//     let username = "Alhamd"
+//         console.log(this.username);
+//     }
+
+// const chai = () => { 
+// let username = "Alhamd"
+//     console.log(this);
+// }
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3,9));
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "Alhamd"})
+console.log(addTwo(3,9))
