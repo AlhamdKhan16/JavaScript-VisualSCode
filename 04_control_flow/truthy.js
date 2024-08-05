942524ff67932ffbc4adaa535f8d8cd8f792ee9c
AlhamdKhan16 <alhamdkhan2003@gmail.com>
2024-08-05 09:37:07 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,13 @@
+const userEmail = "a@alhamd.ai"
+if (userEmail) {
+    console.log("got user Email");
+} else{
+    console.log("Don't have user Email");
+    
+}
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+"0", 'false', " "
